50427ea1ce7b63bb028ffedce09e53215c18b9e2
seanmanik <seanmanik@gmail.com>
2024-09-22 03:54:01 +0800

    Add confirmation

--- usdc-swapping-frontend/src/components/SwapConfirming.js
+++ usdc-swapping-frontend/src/components/SwapConfirming.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './SwapConfirmation.css';  // Import the CSS for styling
+import './SwapConfirming.css';  // Import the CSS for styling
 
 function SwapConfirming() {
-    return (
-        <div className="home-container">
-          <Link to="/confirmation" className="link-style">
-            <button className="home-button-2">
-              <div className="options-title">Swapping</div>
-              <div className="internal-button">
-                <div className="left-side">
-                  <div className="internal-title">Singapore Dollar</div>
-                  <div className="internal-desc">SGD/USDC ~1.277</div>
-                </div>
-                <img src="/singapore.jpeg" alt="Icon 1" className="icon-image round-image"/>
-              </div>
-            </button>
-          </Link>
-        </div>
-      );
-    }
+  return (
+    <div className="home-container">
+      <Link to="/confirming" className="link-style">
+        <button className="home-button-3s">
+          <div className="content-container">
+            <img src="/usdc.png" alt="Coin" className="coin-image"/>
+            <div className="amount-text">1000 USDC</div>
+          </div>
+          <div className="action-text">Select Swap</div>
+        </button>
+      </Link>
+    </div>
+  );
+}
 
 export default SwapConfirming;
